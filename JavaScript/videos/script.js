be799b54c97f90307a36ebbcdff72c0b83b018ca
Mike <maflores86@gmail.com>
2026-03-30 23:07:55 -0300

    feat: fecha dinamica en footer

--- JavaScript/videos/script.js
+++ JavaScript/videos/script.js
@@ -4,3 +4,8 @@ var edad = prompt("¿Qué edad tenés?", 29);
 alert("Hola, tu edad es de "+edad+" años");
 
 alert("el tipo de dato de la 'variable edad' es "+typeof edad);
+
+<script>
+    // Selecciona el elemento y le asigna el año actual automáticamente
+    document.getElementById("year").textContent = new Date().getFullYear();
+</script>
